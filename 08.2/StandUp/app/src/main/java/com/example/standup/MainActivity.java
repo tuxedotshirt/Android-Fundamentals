c104eab105ba1b5c64ccdc79acc0d632c185f370
tuxedotshirt <don.hagen@hotmail.com>
2020-10-07 16:54:15 -0600

    update

--- 08.2/StandUp/app/src/main/java/com/example/standup/MainActivity.java
+++ 08.2/StandUp/app/src/main/java/com/example/standup/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.standup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.widget.CompoundButton;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NotificationManager mNotificationManager;
+
+    private static final int NOTIFICATION_ID = 0;
+    private static final String PRIMARY_CHANNEL_ID =
+            "primary_notification_channel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,18 +31,88 @@ public class MainActivity extends AppCompatActivity {
 
         ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
 
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        Intent notifyIntent = new Intent(this, AlarmReceiver.class);
+
+        boolean alarmUp = (PendingIntent.getBroadcast(this, NOTIFICATION_ID, notifyIntent,
+                PendingIntent.FLAG_NO_CREATE) != null);
+        alarmToggle.setChecked(alarmUp);
+        final PendingIntent notifyPendingIntent = PendingIntent.getBroadcast
+                (this, NOTIFICATION_ID, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        final AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
         alarmToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                String toastMessage;
+                //String toastMessage;
+
                 if(isChecked){
-                    toastMessage = getString(R.string.alarm_on_toast);
+                    long repeatInterval = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
+                    long triggerTime = SystemClock.elapsedRealtime()
+                            + repeatInterval;
+
+//If the Toggle is turned on, set the repeating alarm with a 15 minute interval
+                    if (alarmManager != null) {
+                        alarmManager.setInexactRepeating
+                                (AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                                        triggerTime, repeatInterval, notifyPendingIntent);
+                    }
+                    //deliverNotification(MainActivity.this);
+                    //toastMessage = getString(R.string.alarm_on_toast);
                 }
                 else{
-                    toastMessage = getString(R.string.alarm_off_toast);
+                    mNotificationManager.cancelAll();
+                    if (alarmManager != null) {
+                        alarmManager.cancel(notifyPendingIntent);
+                    }
+                    //toastMessage = getString(R.string.alarm_off_toast);
                 }
-                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_LONG).show();
+                //Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_LONG).show();
             }
         });
+        createNotificationChannel();
+    }
+
+    public void createNotificationChannel(){
+        // Create a notification manager object.
+        mNotificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        // Notification channels are only available in OREO and higher.
+        // So, add a check on SDK version.
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+
+            // Create the NotificationChannel with all the parameters.
+            NotificationChannel notificationChannel = new NotificationChannel
+                    (PRIMARY_CHANNEL_ID,
+                            "Stand up notification",
+                            NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription
+                    ("Notifies every 15 minutes to stand up and walk");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private void deliverNotification(Context context){
+        Intent contentIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentPendingIntent = PendingIntent.getActivity(context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stand_up)
+                .setContentTitle(getString(R.string.stand_up_alert))
+                .setContentText(getString(R.string.stand_up_message))
+                .setContentIntent(contentPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 }
