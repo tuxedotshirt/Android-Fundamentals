50a2c59cb636aae2695689e3ebde3ced41fae057
tuxedotshirt <don.hagen@hotmail.com>
2020-10-08 03:00:57 -0600

    10_1 task 5 finished

---
+++ 10.1/RoomWordSample/app/src/main/java/com/example/roomwordsample/WordRoomDatabase.java
@@ -0,0 +1,28 @@
+package com.example.roomwordsample;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Word.class}, version = 1, exportSchema = false)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    public abstract WordDao wordDao();
+    private static WordRoomDatabase INSTANCE;
+
+    private static WordRoomDatabase getDatabase(final Context context){
+        if(INSTANCE == null){
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    // Create database here
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database").fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
